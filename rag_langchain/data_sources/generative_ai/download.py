3e9d20d559aaa2447943b11503af7f0f614c97a9
ShouyiLeee <22521576@gm.uit.edu.vn>
2024-11-23 01:35:10 +0700

    'update'

--- rag_langchain/data_sources/generative_ai/download.py
+++ rag_langchain/data_sources/generative_ai/download.py
@@ -33,4 +33,4 @@ def is_exist (file_link):
 
 for file_link in file_links :
     if not is_exist(file_link):
-        wget.download(file_link[" url"], out=f"./{file_link['title']}.pdf")
+        wget.download(file_link["url"], out=f"./{file_link['title']}.pdf")
